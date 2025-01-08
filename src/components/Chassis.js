3e5437eaa06668c58e064bf87bb4352de2893ce1
Yan <ygonz42@gmail.com>
2025-01-08 16:32:11 -0500

    added all parts for Base 8800 chassis

--- src/components/Chassis.js
+++ src/components/Chassis.js
@@ -6,6 +6,13 @@ const Chassis = ({ position}) => {
   // Load the base chassis model
   const Chassis8800 = useLoader(GLTFLoader, "/models/ChassisBlank.gltf");
   const Latchbolt8800 = useLoader(GLTFLoader, "/models/Latchbolt8800.gltf");
+  const Latchbolt8800SpringPin = useLoader(GLTFLoader, "/models/Latchbolt8800SpringPin.gltf");
+  const Latchbolt8800SpirolPin = useLoader(GLTFLoader, "/models/latchboltSpirolPin.gltf");
+  const Latchbolt8800PivotPin = useLoader(GLTFLoader, "/models/leverpivotpin.gltf");
+  const LiftLeverRH8800 = useLoader(GLTFLoader, "/models/liftleverRH.gltf");
+  const LiftLeverLH8800 = useLoader(GLTFLoader, "/models/liftleverLH.gltf");
+  const LatchboltSpring8800 = useLoader(GLTFLoader, "/models/latchboltspring.gltf");
+  const OutsideCylHub8800 = useLoader(GLTFLoader, "/models/Cylinderspindlehub8800.gltf");
 
   return (
     <group position={position}>
@@ -17,9 +24,33 @@ const Chassis = ({ position}) => {
         scale={[30, 30, 30]} // Adjust scale to match your scene
       />
       <primitive
-        rotation={[Math.PI/2, 0, Math.PI]} // Default no rotation
+        rotation={[Math.PI/2, Math.PI, 3*Math.PI/2+1]} // Default no rotation
         object={Latchbolt8800.scene}
-        position={[0, 41, 0]} // Adjust as necessary
+        position={[0.7, 41, 0.85]} // Adjust as necessary
+        scale={[30, 30, 30]} // Adjust scale to match your scene
+      />
+            <primitive
+        rotation={[0, Math.PI/2, Math.PI/2]} // Default no rotation
+        object={Latchbolt8800SpringPin.scene}
+        position={[0.3575, 40.75, 0.675]} // Adjust as necessary
+        scale={[30, 30, 30]} // Adjust scale to match your scene
+      />
+                  <primitive
+        rotation={[3*Math.PI/2, 0, 0]} // Default no rotation
+        object={Latchbolt8800SpirolPin.scene}
+        position={[0.7375, 40.35, 0.85]} // Adjust as necessary
+        scale={[30, 30, 30]} // Adjust scale to match your scene
+      />
+                        <primitive
+        rotation={[3*Math.PI/2, 0, 3*Math.PI/2]} // Default no rotation
+        object={Latchbolt8800PivotPin.scene}
+        position={[-.56, 42.2, 0.256]} // Adjust as necessary
+        scale={[30, 30, 30]} // Adjust scale to match your scene
+      />
+                              <primitive
+        rotation={[0, 3*Math.PI/2, 3*Math.PI/2]} // Default no rotation
+        object={LiftLeverRH8800.scene}
+        position={[-.475, 39.795, 0.256]} // Adjust as necessary
         scale={[30, 30, 30]} // Adjust scale to match your scene
       />
 
